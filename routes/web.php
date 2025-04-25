56f854ca9ee4ec04ddef8b81b3e4aa005f10f659
Jay Millena <jay.millena07@gmail.com>
2025-04-25 17:40:38 +0800

    Routing exercise was done

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main page';
+});
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
 });
