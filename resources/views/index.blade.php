736b8af5af4e34be1527b36065953b40b0faa0ca
Jay Millena <jay.millena07@gmail.com>
2025-04-26 14:05:13 +0800

    Using forelse directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,9 @@
 <h1>The list of tasks</h1>
 
 <div>
-    @if(count($tasks))
-        @foreach ($tasks as $task)
-            <div>{{ $task->title }}</div>
-        @endforeach
-    @else
+    @forelse ($tasks as $task)
+        <div>{{ $task->title }}</div>
+    @empty
         <div>There are no tasks!</div>
-    @endif
+    @endforeach
 </div>
