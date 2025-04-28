2bf742e4dd43cc3ab7bb1480acbe49b1255c8b12
Jay Millena <jay.millena07@gmail.com>
2025-04-28 20:43:55 +0800

    Added template inheritance feature

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,14 @@
-<h1>The list of tasks</h1>
+@extends('layouts.app')
 
-<div>
-    @forelse ($tasks as $task)
-        <div><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></div>
-    @empty
-        <div>There are no tasks!</div>
-    @endforelse
-</div>
+@section('title', 'The list of tasks')
+@section('content')
+    <h1>The list of tasks</h1>
+
+    <div>
+        @forelse ($tasks as $task)
+            <div><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></div>
+        @empty
+            <div>There are no tasks!</div>
+        @endforelse
+    </div>
+@endsection
