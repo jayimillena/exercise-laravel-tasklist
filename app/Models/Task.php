f76dc440c286f8f8d00bbc3f19afb0d17681bf0a
Jay Millena <jay.millena07@gmail.com>
2025-05-10 23:20:26 +0800

    Factory and seeder implementation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;    
 
 class Task extends Model
 {
-    //
+    use HasFactory, Notifiable;
 }
